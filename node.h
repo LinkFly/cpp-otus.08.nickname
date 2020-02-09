55a9807d5f1e7df23e34ea8ce6f03943f6f1c2bb
Sergey Katrevich <linkfly@yandex.ru>
2020-02-09 21:31:21 +0300

    big refactoring Node

--- node.h
+++ node.h
@@ -1,66 +1,66 @@
 ﻿#pragma once
 
 #include <string>
-#include <bitset>
 #include <array>
-//#include <type_traits>
 
 using std::string;
 
-// constexpr uint8_t size = 26 + 33;  // eng + rus
-constexpr uint8_t size = 255; // any byte (any codepage)
-//constexpr uint16_t size = 65535; // any byte (any codepage)
+const uint8_t size = 255; // any byte (any codepage)
 
 struct Node {
 	using IdxChar = char;
-	using children_size_t = decltype(size);
+	using children_size_t = std::remove_const<decltype(size)>::type;
 	string label;
 	bool isEnd{};
+	children_size_t childrenCount = 0;
 	std::array<std::unique_ptr<Node>, size> children;
-	// TODO!! Пересмотреть использовать bitset'a
-	std::bitset<size> busySet = 0;
+
+	bool isEmpty(const std::unique_ptr<Node>& node) {
+		return node.get() == nullptr;
+	}
 
 	std::unique_ptr<Node>& getNode(IdxChar ch = 0) {
 		checkChar(ch);
-
-		// TODO!!!!! Возможно Упразднить bitset - считать занятость/не занятость по наличию null/not null, в соотв. ячейке
-		children_size_t& idx = ch;
-		if (busySet[idx]) {
-			return children[idx];
+		children_size_t& idx = reinterpret_cast<children_size_t&>(ch);
+		auto& curNode = children[idx];
+		if (!isEmpty(curNode)) {
+			return curNode;
 		}
 		else {
-			busySet.set(idx);
 			auto newNode = std::make_unique<Node>();
-			children[idx].swap(newNode);
-			return children[idx];
+			return setNode(idx, newNode);
 		}
 	}
+
+	std::unique_ptr<Node>& setNode(IdxChar ch, std::unique_ptr<Node>& node) {
+		children_size_t& idx = reinterpret_cast<children_size_t&>(ch);
+		auto& curNode = children[idx];
+		if (!isEmpty(node)) {
+			if (isEmpty(curNode)) {
+				++childrenCount;
+			}
+		}
+		curNode.swap(node);
+		return curNode;
+	}
+	
 	void forEach(std::function<void(std::unique_ptr<Node>&, children_size_t idx, bool isLast)> fn) {
-		auto count = busySet.count();
-		decltype(count) order = 0;
-		for (std::remove_const<children_size_t>::type i = 0; i < static_cast<children_size_t>(busySet.size()); ++i) {
-			if (busySet[i]) {
-				order++;
-				fn(children[i], i, order == count);
+		decltype(childrenCount) order = 0;
+		for (std::remove_const<children_size_t>::type i = 0; i < static_cast<children_size_t>(children.size()); ++i) {
+			std::unique_ptr<Node>& node = children[i];
+			if (!isEmpty(node)) {
+				++order;
+				fn(node, i, order == childrenCount);
 			}
 		}
 	}
 
-	void setNode(IdxChar ch, std::unique_ptr<Node>& node) {
-		children_size_t& idx = ch;
-		if (node.get() != nullptr) {
-			busySet.set(idx);
-		}
-		children[idx].swap(node);
-	}
-
 	bool isNoChildren() {
-		return busySet.count() == 0;
+		return childrenCount == 0;
 	}
 
 private:
 	static void checkChar(IdxChar ch) {
-		// TODO! Correct error handling
 		if (ch == 0) {
 			error("Bad char\n");
 		}
